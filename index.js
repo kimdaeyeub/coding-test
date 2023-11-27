bdaf38c1f6a793af0b4cc87cd5306e3e5dcefe64
kimdaeyeub <kdy3915@naver.com>
2023-11-27 23:16:36 +0900

    잃어버린 괄호

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim();
 
 function solution(value) {
-  const arr = value.split("+|-");
-  console.log(arr);
+  const arr = value.split("+");
+  if (arr.length === 1) {
+    // 식이 마이너스(-)를 포함하지 않는 경우
+    return Number(eval(value));
+  } else {
+    //const formula = arr.map((item) => item.split("+"));
+    const formula = arr.map((item) => eval(parseInt(item, 10))).join("-");
+    return Number(eval(formula));
+  }
 }
 
 console.log(solution(input));
