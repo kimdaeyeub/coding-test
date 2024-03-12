b041917d718ecb6b0cf9d65a6d3372ba78d84566
kimdaeyeub <kdy3915@naver.com>
2024-03-13 00:56:46 +0900

    10815번 문제풀이 추가

---
+++ 2024-3-13/10815.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
+const n = Number(input[0]);
+const a = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+const m = Number(input[2]);
+const b = input[3].split(" ").map(Number);
+
+function solution(target, arr, start, end) {
+  while (start <= end) {
+    let mid = parseInt((start + end) / 2);
+    if (arr[mid] === target) return 1;
+    else if (arr[mid] > target) end = mid - 1;
+    else start = mid + 1;
+  }
+  return 0;
+}
+
+const result = b.map((v) => solution(v, a, 0, n - 1));
+
+console.log(result.join(" "));
