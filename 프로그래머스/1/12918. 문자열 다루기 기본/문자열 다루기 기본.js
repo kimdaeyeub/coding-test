0fe2df9eeb410efb74728950fdd42d72faa48992
kimdaeyeub <kdy3915@naver.com>
2024-03-20 02:40:33 +0900

    수정

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,14 +1,15 @@
 function solution(s) {
-    if (s.length !== 4 && s.length !== 6) {
-        return false;
+  if (s.length !== 4 && s.length !== 6) {
+    return false;
+  }
+
+  let arr = s.split("");
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!parseInt(arr[i]) && parseInt(arr[i]) !== 0) {
+      return false;
     }
-    for (let i = 0; i < s.length; i++) {
-        const code = s.charCodeAt(i);
-        // ASCII 코드값을 사용하여 숫자가 아닌 문자가 있는지 확인
-        if (code < 48 || code > 57) {
-            return false;
-        }
-    }
-   
-    return true;
+  }
+
+  return true;
 }
