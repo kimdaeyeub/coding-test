4e0b5630953ba3cf786e8ad283e1e241ea1353fa
김대엽 <113541880+kimdaeyeub@users.noreply.github.com>
2024-03-20 01:33:20 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.60 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,25 @@
+function solution(left, right) {
+    var answer = 0;
+    let arr=[]
+    for(let i=left;i<=right;i++){
+        arr.push(i)
+    }
+    let minus=[]
+    let plus=[]
+    for(let i=0;i<arr.length;i++){
+        const num = arr[i]
+        let count=0
+        for(let i=1;i<=num;i++){
+            if(num%i===0){
+                count++
+            }
+        }
+        if(count%2===0){
+            plus.push(num)
+        }else{
+            minus.push(num)
+        }
+    }
+    answer=plus.reduce((a,b)=>a+b)-minus.reduce((a,b)=>a+b)
+    return answer;
+}
