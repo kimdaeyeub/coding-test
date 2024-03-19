557f7f2aca82a463d202fba4a0f604583ba22db7
김대엽 <113541880+kimdaeyeub@users.noreply.github.com>
2024-03-20 00:49:33 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,9 @@
+function solution(phone_number) {
+    var answer = '';
+    let val=phone_number.split("")
+    for(let i=0;i<val.length-4;i++){
+        val[i]="*"
+    }
+    answer=val.join("")
+    return answer;
+}
