41a84b7a145e9460888699bcb525e767fe09d00c
김대엽 <113541880+kimdaeyeub@users.noreply.github.com>
2024-03-20 16:06:36 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    var answer = '';
+    let arr=s.split(" ").map(Number)
+    const min=Math.min(...arr)
+    const max=Math.max(...arr)
+    answer=`${min} ${max}`
+    return answer;
+}
