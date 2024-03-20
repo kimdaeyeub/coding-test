8c212bc6a055f30ab664ae6455de3bb7f08346a2
김대엽 <113541880+kimdaeyeub@users.noreply.github.com>
2024-03-20 16:55:10 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.72 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,12 @@
+function solution(A,B){
+    var answer = 0;
+    B=B.sort((a,b)=>a-b)
+    A=A.sort((a,b)=>b-a)
+    
+    for(let i=0;i<A.length;i++){
+        answer+=A[i]*B[i]
+    }
+
+
+    return answer;
+}
