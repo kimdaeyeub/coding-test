0434dd6e1c77f19986e18e33b47040a9857f9634
김대엽 <113541880+kimdaeyeub@users.noreply.github.com>
2024-03-22 20:33:09 +0900

    [level 2] Title: 소수 찾기, Time: 8.49 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,41 @@
+function makeNumbers(nums,visited,depth,maxDepth,selected,arr){
+    if(depth===maxDepth){
+        if(selected.join("")!=="1"&& !arr.includes(Number(selected.join("")))) {
+            arr.push(Number(selected.join("")))
+        }
+    }
+    for(let i=0;i<nums.length;i++){
+        if(visited[i] ) continue;
+        if(selected.length===0&&nums[i]===0)continue
+        visited[i]=true
+        selected.push(nums[i])
+        makeNumbers(nums,visited,depth+1,maxDepth,selected,arr)
+        visited[i]=false
+        selected.pop()
+    }
+}
+
+function solution(numbers) {
+    var answer = 0;
+    const nums=numbers.split("").map(Number)
+    let arr=[]
+    let selected=[]
+    let visited=new Array(10^(nums.length)).fill(false)
+    
+    for(let i=1;i<=numbers.length;i++){
+        makeNumbers(nums,visited,0,i,selected,arr)
+    }
+    for(let x of arr){
+        let count=0
+        for(let i=2;i<=x;i++){
+            if(x%i===0){
+                count++
+            }
+        }
+        if(count===1){
+            answer++
+        }
+        
+    }
+    return answer;
+}
