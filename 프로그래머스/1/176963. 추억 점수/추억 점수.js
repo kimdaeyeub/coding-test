0579821f76c1cf7963268e55c0a9ebda5e759eb5
김대엽 <113541880+kimdaeyeub@users.noreply.github.com>
2024-03-26 23:03:02 +0900

    [level 1] Title: 추억 점수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,18 @@
+function solution(name, yearning, photo) {
+    var answer = [];
+    let obj={}
+    for(let i=0;i<name.length;i++){
+        obj[name[i]]=yearning[i]
+    }
+    for(let i=0;i<photo.length;i++){
+        let score=0
+        for(let j=0;j<photo[i].length;j++){
+            let peo=photo[i][j]
+            if(!obj[peo]) continue
+            score+=obj[peo]
+        }
+        answer.push(score)
+    }
+
+    return answer;
+}
