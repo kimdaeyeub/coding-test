cb364b1ec72408f824d025b1ec9ac4710151b20b
kimdaeyeub <kdy3915@naver.com>
2024-08-23 17:40:04 +0900

    Revert "크레인 인형뽑기 게임"
    This reverts commit c88c3e7f27b240ed77edc80dd3820a9e2d6123d1.

--- index.js
+++ index.js
@@ -1,41 +1,21 @@
-function solution(board, moves) {
-  let answer = 0;
-  let stacks = [];
-  let basket = [];
+function solution (prices){
+  let stack=[]
+  let arr=new Array(prices.length).fill(0)
 
-  for (let i of board) {
-    stacks.push([]);
+  for(let i=0;i<prices.length;i++){
+      while(stack.length>0 && prices[stack[stack.length-1]]>prices[i]){
+        console.log("hello")
+        let j=stack.pop()
+        arr[j]=i-j
+      }
+      stack.push(i)
   }
 
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board.length; j++) {
-      if (board[i][j] !== 0) stacks[j].push(board[i][j]);
-    }
+  while(stack.length>0){
+    let j=stack.pop()
+    arr[j]=prices.length-1-j
   }
-
-  for (let x of moves) {
-    if (stacks[x - 1].length === 0) continue;
-    const a = stacks[x - 1].shift();
-    if (basket[basket.length - 1] === a) {
-      basket.pop();
-      answer++;
-    } else {
-      basket.push(a);
-    }
-  }
-
-  return answer * 2;
+  return arr
 }
 
-console.log(
-  solution(
-    [
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 0, 3],
-      [0, 2, 5, 0, 1],
-      [4, 2, 4, 4, 2],
-      [3, 5, 1, 3, 1],
-    ],
-    [1, 5, 3, 5, 1, 2, 1, 4]
-  )
-); //4
+console.log(solution([1,2,3,2,3]))
