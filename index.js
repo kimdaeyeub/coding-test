62b855dbf9ff8bbaeae3cf4e1a0d77e7e1565b33
kimdaeyeub <kdy3915@naver.com>
2024-08-23 17:40:04 +0900

    크레인 인형뽑기 게임

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-function solution (prices){
-  let stack=[]
-  let arr=new Array(prices.length).fill(0)
+function solution(board, moves) {
+  let answer = 0;
+  let stacks = [];
+  let basket = [];
 
-  for(let i=0;i<prices.length;i++){
-      while(stack.length>0 && prices[stack[stack.length-1]]>prices[i]){
-        console.log("hello")
-        let j=stack.pop()
-        arr[j]=i-j
-      }
-      stack.push(i)
+  for (let i of board) {
+    stacks.push([]);
   }
 
-  while(stack.length>0){
-    let j=stack.pop()
-    arr[j]=prices.length-1-j
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      if (board[i][j] !== 0) stacks[j].push(board[i][j]);
+    }
   }
-  return arr
+
+  for (let x of moves) {
+    if (stacks[x - 1].length === 0) continue;
+    const a = stacks[x - 1].shift();
+    if (basket[basket.length - 1] === a) {
+      basket.pop();
+      answer++;
+    } else {
+      basket.push(a);
+    }
+  }
+
+  return answer * 2;
 }
 
-console.log(solution([1,2,3,2,3]))
+console.log(
+  solution(
+    [
+      [0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 3],
+      [0, 2, 5, 0, 1],
+      [4, 2, 4, 4, 2],
+      [3, 5, 1, 3, 1],
+    ],
+    [1, 5, 3, 5, 1, 2, 1, 4]
+  )
+); //4
