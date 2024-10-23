489beb0282c2e84b0033b1c0e2d0e4538d3408e0
kimdaeyeub <kdy3915@naver.com>
2024-10-23 17:58:45 +0900

    11726번 문제풀이 추가

---
+++ 2024-10-23/11726.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("./input.txt").toString().trim();
+const N = Number(input);
+
+let arr = new Array(N + 1).fill(0);
+arr[1] = 1;
+arr[2] = 2;
+
+function solution(n) {
+  if (arr[n] !== 0) {
+    return arr[n];
+  }
+
+  let total = (solution(n - 1) + solution(n - 2)) % 10007;
+  arr[n] = total;
+  return total;
+}
+
+console.log(solution(N));
