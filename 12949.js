c6021f210f7becf04164be4a943bcc00f1ea4e7a
kimdaeyeub <kdy3915@naver.com>
2024-12-02 12:52:40 +0900

    12949번 문제풀이 추가

---
+++ 12949.js
@@ -0,0 +1,46 @@
+function solution(arr1, arr2) {
+  let result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    let temp = [];
+    for (let j = 0; j < arr2[0].length; j++) {
+      let val = 0;
+      for (let k = 0; k < arr2.length; k++) {
+        val += arr1[i][k] * arr2[k][j];
+      }
+      temp.push(val);
+    }
+    result.push(temp);
+  }
+  return result;
+}
+
+console.log(
+  solution(
+    [
+      [1, 4],
+      [3, 2],
+      [4, 1],
+    ],
+    [
+      [3, 3],
+      [3, 3],
+    ]
+  )
+); // [[15, 15], [15, 15], [15, 15]]
+
+console.log(
+  solution(
+    [
+      [2, 3, 2],
+      [4, 2, 4],
+      [3, 1, 4],
+    ],
+    [
+      [5, 4, 3],
+      [2, 4, 1],
+      [3, 1, 1],
+    ]
+  )
+); // [[22, 22, 11], [36, 28, 18], [29, 20, 14]]
+
+// 10+6+6 8+12+2 6+3+2
